c9c6016d6045f63927de774b35eb495bb725970e
Alexis Loza <arquidec@gmail.comn>
2020-06-04 01:24:57 -0300

    Add  routes

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+require('express-async-errors');
+
+module.exports = function({HomeRoutes}){
+    const router = express.Router();
+    const apiRoutes = express.Router();
+
+    apiRoutes
+    .use(express.json())
+    .use(cors())
+    .use(helmet()).
+    use(compression());
+
+
+    apiRoutes.use('/home', HomeRoutes)
+}
--- src/startup/container.js
+++ src/startup/container.js
@@ -3,11 +3,26 @@ const {createContainer,asClass, asValue, asFunction}= require('awilix');
 // services
 const {HomeService} = require('../services')
 
+// controllers
+const {HomeController} = require('../controllers')
+
+// Routes
+const {HomeRoutes} = require('../routes/index.routes')
+
 const container = createContainer;
 
 // container register
-container.register({
+container
+.register({
     HomeService: asClass(HomeService).singleton()
 })
+.register({
+    HomeController: asClass(HomeController.bind(HomeController)).singleton()
+})
+.register({
+    HomeRoutes: asFunction(HomeRoutes).singleton()
+})
+
+
 
 module.exports = container;
