5dbaab7bd3fb453b1cf1d1035c1793732ff6c191
Alexis Loza <arquidec@gmail.comn>
2020-06-06 20:07:09 -0300

    [ADD] Services added

--- src/startup/container.js
+++ src/startup/container.js
@@ -5,7 +5,7 @@ const config = require('../config');
 const app = require('.');
 
 // services
-const {HomeService} = require('../services');
+const {HomeService,IdeaService, CommentService, UserService} = require('../services');
 
 // controllers
 const {HomeController} = require('../controllers');
@@ -31,7 +31,11 @@ container
     config: asValue(config)
   })
 .register({
-    HomeService: asClass(HomeService).singleton()
+    HomeService: asClass(HomeService).singleton(),
+    CommentService: asClass(CommentService).singleton(),
+    IdeaService: asClass(IdeaService).singleton(),
+    UserService: asClass(IdeaService).singleton()
+
 })
 .register({
     HomeController: asClass(HomeController.bind(HomeController)).singleton()
